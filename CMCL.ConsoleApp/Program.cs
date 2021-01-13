2100bba40e3376499d22f21e60828f19632e6851
removable <me@imguan.com>
2021-01-13 10:44:29 +0800

    控制台启动器

--- CMCL.ConsoleApp/Program.cs
+++ CMCL.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
 using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -27,7 +29,8 @@ namespace CMCL.ConsoleApp
             {
                 var input = Console.ReadLine()?.ToLower();
                 if (string.IsNullOrWhiteSpace(input)) return;
-                switch (input)
+                var cg = input.Split(' ');
+                switch (cg[0])
                 {
                     case "exit":
                         Console.WriteLine("正在退出...");
@@ -43,7 +46,14 @@ namespace CMCL.ConsoleApp
                         PrintConfig();
                         break;
                     case "versions":
-                        await PrintVersionList();
+                        if (cg.Length == 2 && cg[1].ToInt(0) > 0)
+                        {
+                            await PrintVersionList(cg[1].ToInt(1));
+                        }
+
+                        break;
+                    default:
+                        Console.WriteLine("未知指令");
                         break;
                 }
             }
@@ -52,16 +62,19 @@ namespace CMCL.ConsoleApp
         /// <summary>
         /// 打印版本列表
         /// </summary>
-        private static async Task PrintVersionList()
+        private static async Task PrintVersionList(int pageNo)
         {
             var mirror = MirrorManager.GetCurrentMirror();
             var versionList = await mirror.Version.LoadGameVersionList(Utils.HttpClientFactory.CreateClient());
-            var table = new ConsoleTable("版本", "发布时间","类型");
-            
-            foreach (var vi in versionList.Versions)
+            var table = new ConsoleTable(
+                $"版本({pageNo.ToString()}/{Math.Round(versionList.Versions.Length / 10d).ToString(CultureInfo.InvariantCulture)})",
+                "发布时间", "类型");
+
+            foreach (var vi in versionList.Versions.Skip((pageNo - 1) * 10).Take(10))
             {
                 table.AddRow(vi.Id, vi.ReleaseTime, vi.Type);
             }
+
             table.Write(Format.Alternative);
         }
 
@@ -70,12 +83,14 @@ namespace CMCL.ConsoleApp
         /// </summary>
         private static void PrintConfig()
         {
-            var table = new ConsoleTable("配置项", "字段名","当前内容");
+            var table = new ConsoleTable("配置项", "字段名", "当前内容");
             var config = AppConfig.GetAppConfig();
             foreach (var propertyInfo in config.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
             {
-                table.AddRow(propertyInfo.GetDescription(), propertyInfo.Name, config.GetPropertyByName(propertyInfo.Name));
+                table.AddRow(propertyInfo.GetDescription(), propertyInfo.Name,
+                    config.GetPropertyByName(propertyInfo.Name));
             }
+
             table.Write(Format.Alternative);
         }
 
@@ -94,6 +109,12 @@ namespace CMCL.ConsoleApp
             {
                 Console.WriteLine($"正在启动，游戏版本：{info.Id}，启动器内核版本：{((Launcher) sender).Version}");
             };
+            launcher.OnGameExit += (sender, info, code) => { Console.WriteLine($"{info.Id}已退出，进程结束码：{code.ToString()}"); };
+            launcher.OnCleanNativesDir += (_, _, _) => { Console.WriteLine("正在清理缓存..."); };
+            launcher.OnCheckLibrariesAndAssets += (_, _, _) => { Console.WriteLine("正在校验文件..."); };
+            launcher.OnMojangLogin += (_, _) => { Console.WriteLine("正在登录Mojang..."); };
+            launcher.OnUnzipNatives += (_, _, _) => { Console.WriteLine("正在解压Natives文件..."); };
+            launcher.OnGameStart += (sender, info) => { Console.WriteLine($"{info.Id}已启动"); };
 
             #endregion
 
@@ -105,7 +126,7 @@ namespace CMCL.ConsoleApp
             Console.WriteLine("输入start启动游戏");
             Console.WriteLine("输入exit退出程序");
             Console.WriteLine("输入config查看配置");
-            Console.WriteLine("输入versions获取版本列表");
+            Console.WriteLine("输入versions $page获取版本列表");
             Console.WriteLine("输入help查看更多指令");
         }
     }
