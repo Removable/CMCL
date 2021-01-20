635b8122887a74e236d5b160c9983c7f05d723db
removable <me@imguan.com>
2021-01-20 17:23:05 +0800

    临时提交

--- CMCL.Wpf/UserControl/GameVersionUc.xaml.cs
+++ CMCL.Wpf/UserControl/GameVersionUc.xaml.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using CMCL.LauncherCore.Download;
 using CMCL.LauncherCore.GameEntities;
 using CMCL.LauncherCore.GameEntities.JsonClasses;
 using CMCL.LauncherCore.Utilities;
 using CMCL.Wpf.Window;
+using ComponentUtil.Common.Data;
 using HandyControl.Controls;
 using HandyControl.Data;
 
@@ -31,12 +33,20 @@ namespace CMCL.Wpf.UserControl
         {
         }
 
-        private async ValueTask LoadGameVersionList()
+        /// <summary>
+        /// 加载版本列表
+        /// </summary>
+        /// <param name="forceRefresh">强制刷新</param>
+        /// <returns></returns>
+        private async ValueTask LoadGameVersionList(bool forceRefresh)
         {
             var mirror = MirrorManager.GetCurrentMirror();
 
-            _gameVersionManifest = await mirror.Version.LoadGameVersionList(Utils.HttpClientFactory.CreateClient());
-            _forgePromos = await mirror.Forge.GetForgeVersionList(Utils.HttpClientFactory.CreateClient());
+            if (forceRefresh)
+            {
+                _gameVersionManifest = await mirror.Version.LoadGameVersionList(Utils.HttpClientFactory.CreateClient());
+                _forgePromos = await mirror.Forge.GetForgeVersionList(Utils.HttpClientFactory.CreateClient());
+            }
 
             var dataTable = new DataTable();
             dataTable.Columns.Add("版本");
@@ -46,9 +56,6 @@ namespace CMCL.Wpf.UserControl
             dataTable.Columns.Add("url");
             foreach (var gameVersionInfo in _gameVersionManifest.Versions)
             {
-                //查找支持该版本的forge
-                var supportedForgeList = _forgePromos.Where(f => f.Build != null && f.Build.McVersion == gameVersionInfo.Id).ToArray();
-
                 var remark = string.Empty;
                 if (gameVersionInfo.Id == _gameVersionManifest.Latest.Release)
                     remark = "(最新稳定版)";
@@ -58,7 +65,9 @@ namespace CMCL.Wpf.UserControl
                 dr["版本"] = gameVersionInfo.Id;
                 dr["发布时间"] = DateTime.Parse(gameVersionInfo.ReleaseTime).ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss");
                 dr["类型"] = $"{gameVersionInfo.Type}{remark}";
-                dr["Forge"] = supportedForgeList.Length > 0 ? "支持" : "-";
+                dr["Forge"] = _forgePromos.Any(f => f.Build != null && f.Build.McVersion == gameVersionInfo.Id)
+                    ? "支持"
+                    : "-";
                 dr["url"] = gameVersionInfo.Url;
                 dataTable.Rows.Add(dr);
                 //从1.13正式版开始支持，更早版本抛弃
@@ -77,11 +86,13 @@ namespace CMCL.Wpf.UserControl
         {
             BtnRefresh.IsEnabled = false;
             BtnDownload.IsEnabled = false;
-            var loadingFrm = LoadingFrm.GetInstance(Application.Current.MainWindow);
-            Dispatcher.BeginInvoke(new Action(() => { loadingFrm.ShowDialog(); }));
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                LoadingFrm.GetInstance(Application.Current.MainWindow).ShowDialog();
+            }));
             try
             {
-                await LoadGameVersionList().ConfigureAwait(false);
+                await LoadGameVersionList(true).ConfigureAwait(false);
             }
             catch (Exception exception)
             {
@@ -92,7 +103,7 @@ namespace CMCL.Wpf.UserControl
             {
                 Dispatcher.BeginInvoke(new Action(() =>
                 {
-                    loadingFrm.Close();
+                    LoadingFrm.GetInstance(Application.Current.MainWindow).Close();
                     BtnRefresh.IsEnabled = true;
                     BtnRefresh.Content = "刷新列表";
                     if (VersionListView.Items.Count > 0)
@@ -233,6 +244,35 @@ namespace CMCL.Wpf.UserControl
             {
                 BtnDownload.IsEnabled = true;
                 BtnRefresh.IsEnabled = true;
+            }
+        }
+
+        /// <summary>
+        /// 选择了一个版本
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void SelectVersion(object sender, SelectionChangedEventArgs e)
+        {
+            if (!(VersionListView.SelectedItem is DataRowView selectVer))
+            {
+                NotifyIcon.ShowBalloonTip("错误", "发生错误", NotifyIconInfoType.Error, "AppNotifyIcon");
+                return;
+            }
+            SpDownloadTypes.Children.Clear();
+            var originMenuItem = new MenuItem {Header = "下载原版"};
+            originMenuItem.Click += BtnDownload_OnClick;
+            SpDownloadTypes.Children.Add(originMenuItem);
+
+            var version = selectVer["版本"].ToString();
+            var forgeVersions = _forgePromos.Where(f => f.Build != null && f.Build.McVersion == version).ToArray();
+            foreach (var fv in forgeVersions)
+            {
+                var forgeMenuItem = new MenuItem {Header = $"下载并安装Forge({fv.Name.Replace("recommended","推荐版").Replace("latest","最新版")})"};
+                originMenuItem.Click += BtnDownload_OnClick;
+                
+                SpDownloadTypes.Children.Add(forgeMenuItem);
             }
         }
     }
